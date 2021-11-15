34caf041bef34441ced7d182867a1e6f8fa3389e
huigod <tenghuiit@163.com>
2021-11-15 15:13:07 +0800

    链表快慢指针

--- src/main/java/com/huigod/链表/_206_反转链表.java
+++ src/main/java/com/huigod/链表/_206_反转链表.java
@@ -3,16 +3,17 @@ package com.huigod.链表;
 /**
  * https://leetcode-cn.com/problems/reverse-linked-list/
  *
- * 原理就是将一个节点插入一个链表中
- * 这里只是另外一个新的链表，从头开始插入
+ * 好理解的双指针
+ * 定义两个指针： prepre 和 curcur ；prepre 在前 curcur 在后。
+ * 每次让 prepre 的 nextnext 指向 curcur ，实现一次局部反转
+ * 局部反转完成之后，prepre 和 curcur 同时往前移动一个位置
+ * 循环上述过程，直至 prepre 到达链表尾部
  *
  */
 public class _206_反转链表 {
 
   /**
    * 非递归的方法
-   * @param head
-   * @return
    */
   public ListNode reverseList(ListNode head) {
 
@@ -42,8 +43,6 @@ public class _206_反转链表 {
    * 递归的方式
    *
    * 递归的方式比较抽象，需要先明白方法所实现的功能，然后再考虑细节问题
-   * @param head
-   * @return
    */
   public ListNode reverseList2(ListNode head) {
 
@@ -56,6 +55,34 @@ public class _206_反转链表 {
 
     return newHead;
 
+  }
+
+  /**
+   * 在遍历链表时，将当前节点的 next 指针改为指向前一个节点。由于节点没有引用其前一个节点，因此必须事先存储其前一个节点。在更改引用之前，还需要存储后一个节点。最后返回新的头引用
+   */
+  public ListNode reverseList3(ListNode head) {
+
+    ListNode prev = null;
+
+    while (head != null) {
+      ListNode next = head.next;
+      head.next = prev;
+      prev = head;
+      head = next;
+    }
+    return prev;
+
+  }
+
+  /**
+   * 反转值
+   */
+  public ListNode reverseList4(ListNode head) {
+    ListNode ans = null;
+    for (ListNode x = head; x != null; x = x.next) {
+      ans = new ListNode(x.val, ans);
+    }
+    return ans;
   }
 
 }
